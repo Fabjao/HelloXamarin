bb7ea11aaa26b0dd1cad57e52a504830b15c136b
logonrm <logonrm@fiap.com.br>
2018-05-17 19:09:39 -0300

    Primeiro Commite

---
+++ XF.Hello/XF.Hello/ConfigPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Hello
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {
+            //BindingContext = App.Config;
+            InitializeComponent();
+        }
+        void EnviarEmail(Object sender, EventArgs e)
+        {
+
+            DisplayAlert("Alerta", "Você está mudando uma opção", "OK");
+        }
+
+    }
+}
